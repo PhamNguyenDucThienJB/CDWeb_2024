9fed0f36f2ed7ae0cfdc99d54da4f5a7d0c3b7bc
Pham Nguyen Duc Thien <20130410@st.hcmuaf.edu.vn>
2024-05-30 22:13:53 +0700

    add ErrorForm

---
+++ fe-ts/src/constant/ErrorForm.ts
@@ -0,0 +1,24 @@
+
+const FIELD_EMPTY = 'FIELD_EMPTY'
+const REQUIRE_EMAIL = 'REQUIRE_EMAIL'
+const REQUIRE_PHONE = 'REQUIRE_PHONE'
+const REPEAT_PASS = 'REPEAT_PASS'
+const PASSWORD = 'PASSWORD'
+const TEXT_ERROR_EMPTY = "Trường này không được trống !"
+const TEXT_REQUIRE_EMAIL = "Trường này phải là email !"
+const TEXT_REQUIRE_PHONE = "Trường này phải là số điện thoại !"
+const TEXT_REPEAT_PASS = "Mật khẩu không trùng khớp !"
+const TEXT_PASS = "Mật khẩu tối thiểu 8 kí tự !"
+
+export {
+    FIELD_EMPTY,
+    REQUIRE_EMAIL,
+    REQUIRE_PHONE,
+    TEXT_ERROR_EMPTY,
+    TEXT_REQUIRE_EMAIL,
+    TEXT_REQUIRE_PHONE,
+    REPEAT_PASS,
+    PASSWORD,
+    TEXT_REPEAT_PASS,
+    TEXT_PASS
+}
