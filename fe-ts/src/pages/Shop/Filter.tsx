ebbfe91b98adb13d31b4ca0eaa7f6369d9fed32f
Pham Nguyen Duc Thien <20130410@st.hcmuaf.edu.vn>
2024-05-31 14:40:26 +0700

    update front end

--- fe-ts/src/pages/Shop/Filter.tsx
+++ fe-ts/src/pages/Shop/Filter.tsx
@@ -19,56 +19,12 @@ const Filter = ({ func, sort, displayItem }: FilterProps) => {
                     <option value={'2'}>Bánh Nhật</option>
                     <option value={'3'}>Bánh Thụy Điển</option>
                     <option value={'4'}>Bánh truyền thống</option>
-                    {/* <option value={'5'}>TERREX</option> */}
+               
                 </select>
             </div>
-            {/* <div className="me-4" style={{ width: '200px' }}>
-                <label className="form-label">Màu sắc :</label>
-                <select onChange={(event) => func(event, 1)} className="form-select">
-                    <option selected value="">
-                        -- Tất cả --
-                    </option>
-                    <option value={'RED'}>Màu đỏ</option>
-                    <option value={'PINK'}>Màu hồng</option>
-                    <option value={'ORANGE'}>Màu cam</option>
-                    <option value={'BLUE'}>Màu xanh dương</option>
-                    <option value={'PURPLE'}>Màu tím</option>
-                    <option value={'GRAY'}>Màu xám</option>
-                    <option value={'BLACK'}>Màu đen</option>
-                    <option value={'WHITE'}>Màu trắng</option>
-                    <option value={'GREEN'}>Màu xanh lá</option>
-                </select>
-            </div> */}
-            {/* <div className="me-4" style={{ width: '200px' }}>
-                <label className="form-label">Kích cỡ :</label>
-                <select onChange={(event) => func(event, 2)} className="form-select">
-                    <option selected value="">
-                        -- Tất cả --
-                    </option>
-                    <option value={'35'}>35</option>
-                    <option value={'36'}>36</option>
-                    <option value={'37'}>37</option>
-                    <option value={'38'}>38</option>
-                    <option value={'39'}>39</option>
-                    <option value={'40'}>40</option>
-                    <option value={'41'}>41</option>
-                    <option value={'42'}>42</option>
-                    <option value={'43'}>43</option>
-                    <option value={'44'}>44</option>
-                    <option value={'45'}>45</option>
-                </select>
-            </div> */}
-            <div className="me-4" style={{ width: '200px' }}>
-                <label className="form-label">Loại bánh :</label>
-                <select onChange={(event) => func(event, 3)} className="form-select">
-                    <option selected value="">
-                        -- Tất cả --
-                    </option>
-                    {/* <option value={'SPORT'}>Thể thao</option> */}
-                    <option value={'CHILDREN'}>Trẻ em</option>
-                    <option value={'ADULT'}>Người lớn</option>
-                </select>
-            </div>
+     
+         
+        
             <div className="me-4" style={{ width: '200px' }}>
                 <label className="form-label">Giá sản phẩm :</label>
                 <select onChange={(event) => sort(event)} className="form-select">
