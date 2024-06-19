9fc982b2bf7378d6d34648c9035e3b742450ad9a
Thien <20130410@st.hcmuaf.edu.vn>
2024-06-19 20:32:08 +0700

    update admin Page

--- fe-ts/src/pages/Admin/Order/OrderList.tsx
+++ fe-ts/src/pages/Admin/Order/OrderList.tsx
@@ -57,6 +57,30 @@ const OrderList = () => {
 
     return (
         <div className="container">
+            <nav style={{ display: 'ruby-text' }}>
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <Link to="/admin" className="nav-link">
+                            Trang chủ
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listProduct" className="nav-link">
+                            Danh sách sản phẩm
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listCustomer" className="nav-link">
+                            Danh sách khách hàng
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listOrder" className="nav-link">
+                            Danh sách đơn hàng
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             `<h1>Danh sách đơn hàng</h1>
             <table className="table table-striped">
                 <thead>
--- fe-ts/src/pages/Admin/Product/ProductList.tsx
+++ fe-ts/src/pages/Admin/Product/ProductList.tsx
@@ -32,6 +32,30 @@ const ProductList = () => {
 
     return (
         <div className="container">
+            <nav style={{ display: 'ruby-text' }}>
+                <ul className="nav-list">
+                    <li className="nav-item">
+                        <Link to="/admin" className="nav-link">
+                            Trang chủ
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listProduct" className="nav-link">
+                            Danh sách sản phẩm
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listCustomer" className="nav-link">
+                            Danh sách khách hàng
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/admin/listOrder" className="nav-link">
+                            Danh sách đơn hàng
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
             <h1>Danh sách sản phẩm</h1>
             <table className="table table-striped">
                 <thead>
